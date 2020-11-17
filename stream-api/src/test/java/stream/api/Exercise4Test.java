e924b7ad9f1563ace877574a5dcb5147f2e6c8c0
Tamas Voros <v-tvoros@hotels.com>
2020-11-17 19:25:59 +0100

    Solve stream api exercise 4

--- stream-api/src/test/java/stream/api/Exercise4Test.java
+++ stream-api/src/test/java/stream/api/Exercise4Test.java
@@ -4,10 +4,12 @@ import common.test.tool.annotation.Easy;
 import common.test.tool.dataset.ClassicOnlineStore;
 import common.test.tool.entity.Customer;
 
+import common.test.tool.entity.Item;
 import org.junit.Test;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.is;
@@ -23,31 +25,35 @@ public class Exercise4Test extends ClassicOnlineStore {
          * Find the first customer who registered this online store by using {@link Stream#findFirst}
          * The customerList are ascending ordered by registered timing.
          */
-        Optional<Customer> firstCustomer = null;
+        Optional<Customer> firstCustomer = customerList.stream().findFirst();
 
         assertThat(firstCustomer.get(), is(customerList.get(0)));
     }
 
     @Easy @Test
-    public void isThereAnyoneOlderThan40() {
+    public void isThereAnyoneOlderThanLimit() {
         List<Customer> customerList = this.mall.getCustomerList();
 
         /**
          * Check whether any customer older than 40 exists or not, by using {@link Stream#anyMatch}
          */
-        boolean olderThan40Exists = true;
+        final int LIMIT = 40;
+        Predicate<Integer> isOlderThanLimit = age -> age > LIMIT;
+        boolean olderThan40Exists = customerList.stream().map(Customer::getAge).anyMatch(isOlderThanLimit);
 
         assertThat(olderThan40Exists, is(false));
     }
 
     @Easy @Test
-    public void isEverybodyOlderThan20() {
+    public void isEverybodyOlderThanLimit() {
         List<Customer> customerList = this.mall.getCustomerList();
 
         /**
          * Check whether all customer are older than 20 or not, by using {@link Stream#allMatch}
          */
-        boolean allOlderThan20 = false;
+        final int LIMIT = 20;
+        Predicate<Integer> isOlderThanLimit = age -> age > LIMIT;
+        boolean allOlderThan20 = customerList.stream().map(Customer::getAge).allMatch(isOlderThanLimit);
 
         assertThat(allOlderThan20, is(true));
     }
@@ -60,7 +66,8 @@ public class Exercise4Test extends ClassicOnlineStore {
          * Confirm that none of the customer has empty list for their {@link Customer.wantToBuy}
          * by using {@link Stream#noneMatch}
          */
-        boolean everyoneWantsSomething = false;
+        Predicate<List<Item>> hasEmptyList = List::isEmpty;
+        boolean everyoneWantsSomething = customerList.stream().map(Customer::getWantToBuy).noneMatch(hasEmptyList);
 
         assertThat(everyoneWantsSomething, is(true));
     }
