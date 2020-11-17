40d76aea0373e9bbee2431a11484fbff564d19cb
Tamas Voros <v-tvoros@hotels.com>
2020-11-17 20:25:26 +0100

    Solve exercise api exercise 2

--- collection-interfaces/src/test/java/collection/interfaces/Exercise2Test.java
+++ collection-interfaces/src/test/java/collection/interfaces/Exercise2Test.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.is;
@@ -29,7 +30,7 @@ public class Exercise2Test {
         /**
          * Try to get from key "Alice" using {@link Map#getOrDefault}. If the key doesn't exist, use 30 as default.
          */
-        Integer defaultVal = null;
+        Integer defaultVal = map.getOrDefault("Alice", 30);
 
         assertThat(defaultVal, is(30));
     }
@@ -41,8 +42,8 @@ public class Exercise2Test {
         /**
          * Try to put 2 entry with key as "Alice" value as 32, key as "Joe" and value as 32 using {@link Map#putIfAbsent}.
          */
-        // map.
-        // map.
+         map.putIfAbsent("Alice", 32);
+         map.putIfAbsent("Joe", 32);
 
         assertThat(map.get("Alice"), is(32));
         assertThat(map.get("Joe"), is(22));
@@ -56,9 +57,9 @@ public class Exercise2Test {
          * Merge 2 entry to {@link map} with key="Alice" value=32, key="Joe" value=32 using {@link Map#merge}.
          * If the value already exist for the key, remap with sum value.
          */
-        BiFunction<Object, Object, Integer> remappingFunction = null;
-        // map.
-        // map.
+        BiFunction<Integer, Integer, Integer> remappingFunction = Integer::sum;
+         map.merge("Alice", 32, remappingFunction);
+         map.merge("Joe", 32, remappingFunction);
 
         assertThat(map.get("Alice"), is(32));
         assertThat(map.get("Joe"), is(54));
@@ -71,8 +72,8 @@ public class Exercise2Test {
         /**
          * Try to increment the value for keys "Joe", "Steven" and "Alice" using {@link Map#computeIfPresent}.
          */
-        BiFunction<Object, Object, Integer> remappingFunction = null;
-        // map.
+        Function<String, Integer> remappingFunction = null;
+         map.computeIfAbsent("Joe", remappingFunction);
         // map.
         // map.
 
